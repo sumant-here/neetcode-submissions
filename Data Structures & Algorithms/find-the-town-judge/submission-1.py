c53f6276ad8dde815a1f4577bf11a33589438eba
sumant-here <sumansumant2005@gmail.com>
2026-09-01 17:55:47 +0530

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        dl = defaultdict(int)
+        for  inc,out in trust:
+            if inc != out:
+                dl[inc] -= 1 # outgi=oing trust 
+                dl[out] += 1 # incoming trust
+        for i in range(1, n + 1):
+            if dl[i] ==  n - 1:
+                return  i 
+        return - 1
